6092203a446bf8a5675d31e92292e4d4d32a55cf
Ekuspreet <ekuspreetsingh2@gmail.com>
2023-02-08 12:31:54 +0530

    harsimran

---
+++ Harsimran-Start-Doing-It/searchHistory.js
@@ -0,0 +1,16 @@
+var inputFeild = document.getElementById("search_Feild");
+var items = document.getElementById("search_History");
+var items_list = document.getElementById("search_List");
+
+var item_value = inputFeild.value();
+function displaySearchItems(){
+items.style.display = "block";
+
+
+}
+function HideDisplaySearchItems(){
+    items.style.display = "none";
+}
+
+myList.innerHTML += "<li>" + newItemValue + "</li>";
+
