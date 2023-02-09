b55c7128a64624d1725561c3aef9d18a55940e7a
CashmereKey <dhruvsethi870@gmail.com>
2023-02-09 13:01:09 +0530

    Products Db

--- daveydark/src/setup_db.py
+++ daveydark/src/setup_db.py
@@ -2,14 +2,14 @@ import pandas as pd
 import sqlite3
 
 # Load the .xlsx file into a pandas DataFrame
-dfSellers = pd.read_excel('daveydark\src\Database.xlsx')
+dfSellers = pd.read_excel('daveydark\src\Products Db.xlsx')
 # dfProducts = pd.read_excel('products.xlsx')
 
 # Connect to an SQLite database
 conn = sqlite3.connect('instance/db.sqlite3')
 
 # Write the DataFrame to the database
-dfSellers.to_sql('sellers', conn, if_exists='replace', index=False)
+dfSellers.to_sql('products', conn, if_exists='replace', index=False)
 # dfProducts.to_sql('products', conn, if_exists='replace', index=False)
 
 # Close the connection to the database
