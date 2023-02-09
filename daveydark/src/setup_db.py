00fd954f6f97c8bec0fbfecfaebd42b7764ac512
DaveyDark <devesh97531@gmail.com>
2023-02-09 13:24:24 +0530

    Finished db setup script

--- daveydark/src/setup_db.py
+++ daveydark/src/setup_db.py
@@ -2,14 +2,22 @@ import pandas as pd
 import sqlite3
 
 # Load the .xlsx file into a pandas DataFrame
-dfSellers = pd.read_excel('daveydark\src\Products Db.xlsx')
+dfProducts = pd.read_excel('daveydark/src/Products Db.xlsx')
+dfSellers = pd.read_excel('daveydark/src/Database.xlsx')
 # dfProducts = pd.read_excel('products.xlsx')
 
 # Connect to an SQLite database
 conn = sqlite3.connect('instance/db.sqlite3')
+cursor = conn.cursor()
+table_names = ['sellers','products']
+for table_name in table_names:
+    cursor.execute(f"DELETE FROM {table_name};")
+conn.commit()
+
 
 # Write the DataFrame to the database
-dfSellers.to_sql('products', conn, if_exists='replace', index=False)
+dfProducts.to_sql('products', conn, if_exists='replace', index=False)
+dfSellers.to_sql('sellers', conn, if_exists='replace', index=False)
 # dfProducts.to_sql('products', conn, if_exists='replace', index=False)
 
 # Close the connection to the database
