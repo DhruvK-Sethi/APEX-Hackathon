977269bfa578f3a4818f4d9320dbf48b7a439293
CashmereKey <dhruvsethi870@gmail.com>
2023-02-09 17:26:44 +0530

    Score Data update

--- daveydark/src/setup_db.py
+++ daveydark/src/setup_db.py
@@ -4,10 +4,12 @@ import sqlite3
 # Load the .xlsx file into a pandas DataFrame
 dfProducts = pd.read_excel('daveydark/src/Products Db.xlsx')
 dfSellers = pd.read_excel('daveydark/src/Database.xlsx')
+dfscores = pd.read_excel('daveydark/src/Prediction Db.xlsx')
 # dfProducts = pd.read_excel('products.xlsx')
 
 # Connect to an SQLite database
-conn = sqlite3.connect('instance/db.sqlite3')
+# conn = sqlite3.connect('instance/db.sqlite3')
+conn = sqlite3.connect('../var/main-instance/db.sqlite3')
 cursor = conn.cursor()
 table_names = ['sellers','products']
 for table_name in table_names:
@@ -18,6 +20,7 @@ conn.commit()
 # Write the DataFrame to the database
 dfProducts.to_sql('products', conn, if_exists='replace', index=False)
 dfSellers.to_sql('sellers', conn, if_exists='replace', index=False)
+dfscores.to_sql('scores', conn, if_exists='replace', index=False)
 # dfProducts.to_sql('products', conn, if_exists='replace', index=False)
 
 # Close the connection to the database
